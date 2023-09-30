f5a20775ccb54d1306a8a5ab99f49208128b47bf
heitorc62 <heitor.c@usp.br>
2023-09-30 16:22:26 -0300

    model: update dataset class and driver code.
    Signed-off-by: heitorc62 <heitor.c@usp.br>

--- data/dataset.py
+++ data/dataset.py
@@ -1,25 +1,47 @@
 import os
+import torch
 import pandas as pd
+from transformers import RobertaTokenizer
 from torch.utils.data import Dataset
 
 class CodeSnifferDataset(Dataset):
     def __init__(self, annotations_file, code_files_dir, transform=None, target_transform=None):
+        self.tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-small')
         self.code_labels = pd.read_csv(annotations_file)
         self.code_files_dir = code_files_dir
         self.transform = transform
         self.target_transform = target_transform
+        self.tokenize_data()
+        self.tokenized_data = torch.load(self.tokenized_data_path)
+
+
+    def tokenize_data(self):
+        self.tokenized_data_path = "tokenized_data.pt" 
+        if os.path.isfile(self.tokenized_data_path):
+            return
+        
+        tokenized_data = []
+        for idx in range(len(self.code_labels)):
+            code_file_path = os.path.join(self.code_files_dir, self.code_labels.iloc[idx, 0])
+            with open(code_file_path, "r") as code_file:   # open text file in read mode
+                code_data = code_file.read()               # read whole file to a string
+                if self.transform:
+                    code_data = self.transform(code_data)
+                input = self.tokenizer(code_data, return_tensors="pt", padding=True, truncation=True, max_length=512)
+                tokenized_data.append(input)
+            
+        torch.save(tokenized_data, self.tokenized_data_path)
+       
+            
 
     def __len__(self):
         return len(self.code_labels)
 
     def __getitem__(self, idx):
-        code_file_path = os.path.join(self.code_files_dir, self.code_labels.iloc[idx, 0])
-        code_file = open(code_file_path, "r")      # open text file in read mode
-        code_data = code_file.read()               # read whole file to a string
-        code_file.close()                          # close file
+        input_ids = self.tokenized_data[idx]["input_ids"]
+        attention_mask = self.tokenized_data[idx]["attention_mask"]
+
         labels = self.code_labels.iloc[idx, 1:]
-        if self.transform:
-            code_data = self.transform(code_data)
         if self.target_transform:
             labels = self.target_transform(labels)
-        return code_data, labels
+        return input_ids, attention_mask, labels
