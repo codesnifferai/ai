37d22c79c7c33aaec66e0619fa5f4845b0d98418
heitorc62 <heitor.c@usp.br>
2023-09-30 17:59:44 -0300

    model: apply activation functions in fc layer.
    Signed-off-by: heitorc62 <heitor.c@usp.br>
    Co-authored-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com>

--- model/sniffer.py
+++ model/sniffer.py
@@ -4,16 +4,17 @@ import torch.nn as nn
 class CodeSnifferNetwork(nn.Module):
     def __init__(self, num_labels):
         super(CodeSnifferNetwork, self).__init__()
-        
+
         self.t5_encoder = T5ForConditionalGeneration.from_pretrained('Salesforce/codet5-small').encoder
-        self.fc = nn.Linear(self.t5_encoder.config.d_model, 512)
-        self.classifier = nn.Linear(512, num_labels)
-        self.sigmoid = nn.Sigmoid()
+        self.fc = nn.Sequential(
+            nn.Linear(self.t5_encoder.config.d_model, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_labels),
+            nn.Sigmoid()
+        )
 
     def forward(self, input_ids, attention_mask=None):
         encoder_outputs = self.t5_encoder(input_ids=input_ids, attention_mask=attention_mask)
         pooled_output = encoder_outputs[0][:, 0]
-        fc_output = self.fc(pooled_output)
-        logits = self.classifier(fc_output)
-        probs = self.sigmoid(logits)
+        probs = self.fc(pooled_output)
         return probs
