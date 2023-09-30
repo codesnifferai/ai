193f3c8d184d8de5649d29d0a41d0bee833a282d
João Gabriel Josephik <joao.gabrielaaj@gmail.com>
2023-09-30 17:12:00 -0300

    web_scraping change_encoding: remove trailing null byte
    Some files has a null byte at the start. This can cause incorrect encoding detection. Remove this byte.
    
    Signed-off-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com>

--- web_scraping/change_encoding.sh
+++ web_scraping/change_encoding.sh
@@ -2,6 +2,12 @@
 
 for filename in ./data/**/*.java; do    
     encoding=$(file -i "$filename" | sed -n -e 's/^.*charset=//p' | tr a-z A-Z | sed 's/ *$//g')
+    if [[ $encoding == "BINARY" ]]; then
+        printf "$filename $encoding \n" 
+        cat "$filename" | tr -d '\000' > converted.txt
+        mv "converted.txt" "$filename"
+        encoding=$(file -i "$filename" | sed -n -e 's/^.*charset=//p' | tr a-z A-Z | sed 's/ *$//g')
+    fi
     if [[ $encoding != "UTF-8" && $encoding != "US-ASCII" ]]; then
         printf "$filename $encoding \n"        
         iconv -f "${encoding}" -t "UTF-8" "$filename" > converted.txt &&
