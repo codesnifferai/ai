4ab4a7b76a568f128ac4e8a8ec5ea864e11d5d64
heitorc62 <heitor.c@usp.br>
2023-09-30 18:04:26 -0300

    model: add training loop.
    Signed-off-by: heitorc62 <heitor.c@usp.br>
    Co-authored-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com>

---
+++ model/train.py
@@ -0,0 +1,99 @@
+import torch
+import copy
+import time
+
+
+def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=3):
+    since = time.time()
+    val_acc_history = []
+    val_loss_history = []
+    train_acc_history = []
+    train_loss_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # temporary variables to store predictions and true labels
+        epoch_preds = []
+        epoch_true = []
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for batch_input_ids, batch_attention_mask, batch_labels in dataloaders[phase]:
+                batch_input_ids = batch_input_ids.to(device)
+                batch_attention_mask = batch_attention_mask.to(device)
+                batch_labels = batch_labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # In train mode we calculate the loss by summing the final output and the auxiliary output
+                    # but in testing we only consider the final output.
+                    batch_outputs = model(batch_input_ids, batch_attention_mask)
+                    loss = criterion(batch_outputs, batch_labels)
+
+                    preds = (batch_outputs > 0.5).double()
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+
+                # statistics
+                running_loss += loss.item() * batch_input_ids.size(0)
+                running_corrects += torch.sum(preds == batch_labels.data)
+
+                # store predictions and true labels
+                # we're only interested in validation performance
+                if phase == 'val':  
+                    epoch_preds.extend(preds.tolist())
+                    epoch_true.extend(batch_labels.data.tolist())
+
+
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} ---> {} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                best_preds, best_true = epoch_preds, epoch_true
+
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+                val_loss_history.append(epoch_loss)
+            else:
+                train_acc_history.append(epoch_acc)
+                train_loss_history.append(epoch_loss)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('{} ---> Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('{} ---> Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, best_preds, best_true, val_acc_history, val_loss_history, train_acc_history, train_loss_history
