3cc9cba7f68c376dd93138ea63ee08102b3106c5
João Gabriel Josephik <joao.gabrielaaj@gmail.com>
2023-09-30 23:33:58 -0300

    web_scraping change_encoding: delete problematic files.
    Some files are being detected as BINARY, even after known workararounds. Delete those files.
    
    Signed-off-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com>

--- web_scraping/change_encoding.sh
+++ web_scraping/change_encoding.sh
@@ -7,8 +7,10 @@ for filename in ./data/**/*.java; do
         cat "$filename" | tr -d '\000' > converted.txt
         mv "converted.txt" "$filename"
         encoding=$(file -i "$filename" | sed -n -e 's/^.*charset=//p' | tr a-z A-Z | sed 's/ *$//g')
-    fi
-    if [[ $encoding != "UTF-8" && $encoding != "US-ASCII" ]]; then
+        if [[ $encoding == "BINARY" ]]; then
+            rm -f -- "$filename"
+        fi        
+    elif [[ $encoding != "UTF-8" && $encoding != "US-ASCII" ]]; then
         printf "$filename $encoding \n"        
         iconv -f "${encoding}" -t "UTF-8" "$filename" > converted.txt &&
         mv "converted.txt" "$filename"
