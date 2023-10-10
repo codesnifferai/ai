14544a971789564680327752fc54434e6cce997b
João Gabriel Josephik <joao.gabrielaaj@gmail.com>
2023-10-10 15:54:59 -0300

    web_scraping remove_license: adds progress bar
    The output of remove_license.py is confusing. Adds progress bar and
    removes unecessary prints.
    
    Signed-off-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com>

--- web_scraping/remove_license.py
+++ web_scraping/remove_license.py
@@ -2,40 +2,33 @@
 import os
 import re
 import sys
+from tqdm import tqdm
 
 if len(sys.argv) < 2:
     print(f"No dataset path provided. \nUsage: {sys.argv[0]} DATASET_PATH")
     exit(1)
 DATASET_DIR = sys.argv[1]
 
-for filename in os.listdir(DATASET_DIR):
+for filename in tqdm(os.listdir(DATASET_DIR)):
     try:        
-        f = os.path.join(DATASET_DIR, filename)
-        # checking if it is a file
-        print(f)
-        if os.path.isfile(f):
-            # print(f)
+        f = os.path.join(DATASET_DIR, filename)       
+        if os.path.isfile(f):            
             with open(f, "r") as file:
                 string = file.read()
             count_comment = 0            
             start = string[0:2]
-            if start != '/*':
-                print(f"Skipping {filename}. It does not contain a license.")
+            if start != '/*':                
                 continue
             end = 2
             while string[end-2:end] != "*/" and end < len(string):
                 end += 1
-            if end==len(string):
-                print("ERROR: Ending of comment block not found")
-            
             string = string[end:]                   
-            print(end)           
+       
             
             with open(f, "w") as file:
                 file.write(string)
     except Exception as e:
         print(e)
-        print(f"Removing {f}.")
         os.remove(f)
 
 
