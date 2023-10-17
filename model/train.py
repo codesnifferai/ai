55ca4dfb4d6fb185f1ed426f0cd5fe85af9bff11
heitorc62 <heitor.c@usp.br>
2023-10-17 10:48:05 -0300

    train code: Fix bug in print statement.

--- model/train.py
+++ model/train.py
@@ -73,7 +73,7 @@ def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=3):
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
 
-            print('{} ---> {} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
             # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
