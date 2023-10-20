75f7493b4d08fd57efa8925e9486b9a51c2976e4
heitorc62 <heitor.c@usp.br>
2023-10-20 14:33:21 -0300

    deployment: Add boilerplate code to make CodeSniffer's inference.

--- model/codesniffer_test.py
+++ model/codesniffer_test.py
@@ -2,13 +2,14 @@ import random
 import sys
 sys.path.append('../')
 from data.dataset import CodeSnifferDataset
-from torch.utils.data import DataLoader
 from model.modules.sniffer import CodeSnifferNetwork
 import torch
 
 random.seed(90)
-batch_size = 16
-workers = 4
+BATCH_SIZE = 16
+WORKERS = 4
+NUM_LABELS=8
+PATH = "models/codeSniffer.pth"
 
 dataset = CodeSnifferDataset('../data/filtered_data.csv', '../data/code_files')
 
@@ -20,18 +21,16 @@ input_ids, attention_mask, labels = dataset[ind]
 
 print("Got one tensor")
 
-data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
-INPUT_IDS, ATTENTION_MASKS, LABELS = next(iter(data_loader))
 
-print("Got batch")
-
-model = CodeSnifferNetwork()
+model = CodeSnifferNetwork(num_labels=NUM_LABELS)
+model.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
 model.eval()
+
 with torch.no_grad():
     print("One tensor: ")
     y_pred = model.forward(input_ids, attention_mask)
-    print(y_pred)
-
-    print("Batch: ")
-    Y_PRED = model.forward(INPUT_IDS, ATTENTION_MASKS)
-    print(Y_PRED)
+    y_pred = y_pred.squeeze(0).tolist()
+    y_pred = [round(num, 2) for num in y_pred]
+    print(f"y_pred = {y_pred}")
+    labels = labels.tolist()
+    print(f"y_true = {labels}")
