2dc380f0c4bc8cb3f5a0c3507e80942411c854e8
heitorc62 <heitor.c@usp.br>
2023-10-20 14:52:58 -0300

    deployment: Add boilerplate code to make inference.

--- model/codesniffer_test.py
+++ model/codesniffer_test.py
@@ -4,14 +4,20 @@ sys.path.append('../')
 from data.dataset import CodeSnifferDataset
 from model.modules.sniffer import CodeSnifferNetwork
 import torch
+import pandas as pd
 
 random.seed(90)
 BATCH_SIZE = 16
 WORKERS = 4
 NUM_LABELS=8
 PATH = "models/codeSniffer.pth"
+LABELS_FILE = "../data/filtered_data.csv"
 
 dataset = CodeSnifferDataset('../data/filtered_data.csv', '../data/code_files')
+
+classes = pd.read_csv(LABELS_FILE).iloc[0]
+
+classes = classes.index.to_list()[1:]
 
 print("Instantiated dataset")
 
@@ -31,6 +37,8 @@ with torch.no_grad():
     y_pred = model.forward(input_ids, attention_mask)
     y_pred = y_pred.squeeze(0).tolist()
     y_pred = [round(num, 2) for num in y_pred]
+    y_pred = dict(zip(classes, y_pred))
     print(f"y_pred = {y_pred}")
-    labels = labels.tolist()
-    print(f"y_true = {labels}")
+    y_true = labels.tolist()
+    y_true = dict(zip(classes, y_true))
+    print(f"y_true = {y_true}")
